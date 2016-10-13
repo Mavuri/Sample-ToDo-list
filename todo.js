013f84c312734d96b3404a1a88b7160fdabdb820
Mavuri <roshan.coolcool@gmail.com>
2016-10-13 15:55:21 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,15 @@
+var defaults = {
+  // CSS selectors and attributes that would be used by the JavaScript functions
+  todoTask: "todo-task",
+  todoHeader: "task-header",
+  todoDate: "task-date",
+  todoDescription: "task-description",
+  taskId: "task-",
+  formId: "todo-form",
+  dataAttribute: "data",
+  deleteDiv: "delete-div"
+}, codes = {
+  "1" : "#pending", // For pending tasks
+  "2" : "#inProgress",
+  "3" : "#completed"
+};
